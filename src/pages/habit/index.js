0b7aba639aab413850704baae7fbf5403f24bce3
Youthink <hufy3651@foxmail.com>
2020-05-19 08:31:19 +0800

    import weui icon

--- src/pages/habit/index.js
+++ src/pages/habit/index.js
@@ -1,15 +1,15 @@
 import * as React          from 'react';
 import { View, Text }      from 'remax/wechat';
 import { pickColorByName } from '@/utils/helper';
+import Icon                from 'weui-miniprogram/miniprogram_dist/icon/icon';
 import './index.scss';
 
-
 const habitList = [
-  { id: 1, name: '💤 午休', score: 2, maxContinueDays: 14 },
-  { id: 2, name: '🍔 吃早餐', score: 3, maxContinueDays: 12 },
-  { id: 3, name: '🛌 早睡', score: 4, maxContinueDays: 54 },
+  { id: 1, name: '💤 午休(精神小伙)', score: 2, maxContinueDays: 14 },
+{ id: 2, name: '🍔 吃早餐(Let`s go!)', score: 3, maxContinueDays: 12 },
+  { id: 3, name: '🛌 早睡(好作息的开始)', score: 4, maxContinueDays: 54 },
   { id: 4, name: '☀️ 早起', score: 5, maxContinueDays: 114 },
-  { id: 5, name: '🐱 铲猫屎', score: 6, maxContinueDays: 1 },
+  { id: 5, name: '🐱 铲猫屎(喵喵)', score: 6, maxContinueDays: 1 },
 ];
 
 export default () => {
@@ -17,9 +17,9 @@ export default () => {
     if (!Array.isArray(lists)) {
       return null
     }
-    const title = type === 'init' ? '未完成的习惯' : '已完成的习惯'
-    const action = type === 'init' ? '' : 'CANCEL'
-    const iconType = type === 'completed' ? 'check' : 'check-circle'
+    const title = type === 'init' ? '未完成的习惯' : '已完成的习惯';
+    const action = type === 'init' ? '' : 'CANCEL';
+    const iconType = type === 'completed' ? 'done' : 'done2';
     return (
       <View className="habits">
         <Text class="status-title">{`${title}（${lists.length || 0} 个）`}</Text>
@@ -41,8 +41,15 @@ export default () => {
                   <Text>连续天数</Text>
                 </Text>
                 }
-                <Text className="operation-btn">
-                </Text>
+                <View className="operation-btn">
+                  <Icon
+                    icon={iconType}
+                    color="rgba(255, 255, 255, .8)"
+                    extClass="habit-check-icon"
+                    type="outline"
+                    size={25}
+                  />
+                </View>
               </View>
             )
           })}
@@ -55,6 +62,7 @@ export default () => {
     <View className="habit-page">
       <View className="view-scroll-y habits-container">
         {renderHabitList(habitList, 'init')}
+        {renderHabitList(habitList, 'completed')}
       </View>
     </View>
   );
