23c368848680a881f535afedca847ef130ae709f
Youthink <hufy3651@foxmail.com>
2020-05-19 08:41:02 +0800

    添加底部导航

--- src/app.config.js
+++ src/app.config.js
@@ -6,5 +6,29 @@ module.exports = {
   window: {
     navigationBarTitleText: 'Remax Wechat Template',
     navigationBarBackgroundColor: '#282c34'
-  }
+  },
+  tabBar: {
+    list: [
+      {
+        "pagePath": "pages/index/index",
+        "text": "首页"
+      },
+      {
+        "pagePath": "pages/habit/index",
+        "text": "习惯"
+      },
+      {
+        "pagePath": "pages/index/index",
+        "text": "奖励"
+      },
+      {
+        "pagePath": "pages/index/index",
+        "text": "小组"
+      },
+      {
+        "pagePath": "pages/habit/index",
+        "text": "我的"
+      }
+    ]
+  },
 };
