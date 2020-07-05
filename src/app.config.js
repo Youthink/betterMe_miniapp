eb3b1f3c40149155e20b99783d6b0949b10d677e
Youthink <hufy3651@foxmail.com>
2020-07-06 00:04:07 +0800

    tabbar

--- src/app.config.js
+++ src/app.config.js
@@ -1,21 +1,27 @@
-module.exports = {
+export default {
   pages: [
     'pages/index/index',
     'pages/habit/index'
   ],
   window: {
-    navigationBarTitleText: 'Remax Wechat Template',
+    navigationBarTitleText: 'betterMe',
     navigationBarBackgroundColor: '#282c34'
   },
   tabBar: {
+    color: "#707070",
+    selectedColor: "#5E125C",
     list: [
       {
-        "pagePath": "pages/habit/index",
-        "text": "习惯"
+        pagePath: "pages/habit/index",
+        iconPath: './assets/sun.png',
+        selectedIconPath: './assets/habit-checked.png',
+        text: "习惯"
       },
       {
-        "pagePath": "pages/index/index",
-        "text": "奖励"
+        pagePath: "pages/index/index",
+        iconPath: './assets/gift.png',
+        selectedIconPath: './assets/award-checked.png',
+        text: "奖励"
       },
       /*
       {
@@ -32,8 +38,5 @@ module.exports = {
         }
        */
     ]
-  },
-  useExtendedLib: {
-    weui: true
   }
 };
